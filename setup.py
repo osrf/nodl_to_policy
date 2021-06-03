916036a22707ba758df1cbe8628068446cf30684
Abrar Rahman Protyasha <abrar@openrobotics.org>
2021-06-03 15:33:59 -0400

    Importing `ubuntu-robotics/nodl` dependencies
    Also:
    
    * Updating some package information in `setup.py`
    * Adding a `.gitignore` from
      (https://github.com/github/gitignore/blob/master/Python.gitignore)
    
    Signed-off-by: Abrar Rahman Protyasha <abrar@openrobotics.org>

--- setup.py
+++ setup.py
@@ -13,9 +13,22 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
+    author='Abrar Rahman Protyasha',
+    author_email='abrar@openrobotics.org',
     maintainer='Abrar Rahman Protyasha',
     maintainer_email='abrar@openrobotics.org',
-    description='Tool to generate a ROS 2 Access Control Policy from the NoDL description of a ROS system',
+    url='https://github.com/aprotyas/nodl_to_policy',
+    download_url='https://github.com/aprotyas/nodl_to_policy/releases',
+    keywords=['ROS'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License 2.0',
+        'Programming Language :: Python',
+    ],
+    description='NoDL description to ROS 2 policy generator',
+    long_description='Package to generate a ROS 2 Access Control Policy from \
+    the NoDL description of a ROS system',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
