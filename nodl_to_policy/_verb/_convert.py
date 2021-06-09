8da137455e1a97825ce7db22551998239a21e268
Abrar Rahman Protyasha <abrar@openrobotics.org>
2021-06-09 19:10:32 -0400

    Added a `ros2cli` entrypoint, and a `convert` verb
    Usage: `ros2 nodl_to_policy convert [NoDL description file(s)]`
    
    The API for the conversion mechanism has not been well-defined yet, but
    will be part of the next steps.
    
    Signed-off-by: Abrar Rahman Protyasha <abrar@openrobotics.org>

---
+++ nodl_to_policy/_verb/_convert.py
@@ -0,0 +1,35 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import importlib.resources
+import argparse
+import pathlib
+
+from argcomplete.completers import FilesCompleter
+import nodl
+from nodl._index import _FILE_EXTENSION as _NODL_FILE_EXTENSION
+from ros2cli.verb import VerbExtension
+
+class _ConvertVerb(VerbExtension):
+    """Convert NoDL XML documents to ROS 2 Access Control Policies"""
+
+    def add_arguments(self, parser: argparse.ArgumentParser, cli_name: None = None) -> None:
+        arg = parser.add_argument('NODL_FILE_PATH', nargs='*', default=[], metavar='file', type=pathlib.Path, help=f'Specific {_NODL_FILE_EXTENSION} file(s) to convert.')
+        arg.completer = FilesCompleter(allowednames=[_NODL_FILE_EXTENSION], directories=False)
+
+        parser.add_argument('-p', '--print', action='store_true', help='Print converted output.')
+
+    def main(self, *, args: argparse.Namespace) -> int:
+        # TO-DO: Make use of "conversion" API to do this
+        return 0
