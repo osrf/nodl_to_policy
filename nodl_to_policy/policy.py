3db40d0ca5689fc0003ed9bc274889f2ff1296f7
Abrar Rahman Protyasha <abrar@openrobotics.org>
2021-06-17 11:51:24 -0400

    "Complete"-ed NoDL conversion API
    Concerns:
    * Enclave paths are not well specified. Is it fair to make an
    assumption and default to `"[node_name][executable_name]"`?
    * Node namespaces are also not well specified. It certainly is
    not fair to assume all nodes belong to the unspecified `"/"`
    general namespace.
    
    Both of the above concerns can be addressed by expanding the
    definition of the node interface description language. This
    discussion probably has a place in ros2/design#266.
    
    TO-DO:
    * Verify whether default enclave paths should be left blank
    or should be set to an unqualified general path ("/")
    * Writing unit and functional tests for the `nodl_to_policy`
    utility.
    
    Signed-off-by: Abrar Rahman Protyasha <abrar@openrobotics.org>

--- nodl_to_policy/policy.py
+++ nodl_to_policy/policy.py
@@ -12,23 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import importlib.resources
 import pathlib
-from typing import List
+from typing import Dict, List, Optional, Tuple
 
 from lxml import etree
 
-import nodl
-from nodl.types import Node
+import nodl  # noqa: F401
+from nodl.types import (
+    Node,
+    PubSubRole,
+    ServerClientRole,
+)
 
 from sros2.policy import (
+    POLICY_VERSION,
     dump_policy,
     load_policy,
-    POLICY_VERSION,
 )
 
 
 _POLICY_FILE_EXTENSION = '.policy.xml'
+
 
 def get_policy(policy_file_path: pathlib.Path) -> etree._ElementTree:
     # Reference: https://github.com/ros2/sros2/blob/e5e90f05c3d800c51648e02d48d30ed4d6158820/sros2/sros2/verb/generate_policy.py#L60-L68
@@ -41,23 +45,79 @@ def get_policy(policy_file_path: pathlib.Path) -> etree._ElementTree:
         policy.append(enclaves)
         return policy
 
+
+def get_profile(policy: etree._ElementTree, node_name: str) -> etree._ElementTree:
+    enclave = policy.find(path=f'enclaves/enclave[@path=""]')
+    if enclave is None:
+        enclave = etree.Element('enclave')
+        # unqualified enclave path for now, refer to https://design.ros2.org/articles/ros2_security_enclaves.html
+        enclave.attrib['path'] = ''
+        profiles = etree.Element('profiles')
+        enclave.append(profiles)
+        enclaves = policy.find('enclaves')
+        enclaves.append(enclave)
+
+    profile = enclave.find(path=f'profiles/profile[@ns=""][@node="{node_name}"]')
+    if profile is None:
+        profile = etree.Element('profile')
+        # namespace information not provided in NoDL description yet
+        profile.attrib['ns'] = ''
+        profile.attrib['node'] = node_name
+        profiles = enclave.find('profiles')
+        profiles.append(profile)
+
+    return profile
+
+
+def get_permission(profile: etree._ElementTree, permission_type: str, rule_type: str, rule_expression: str) -> etree._ElementTree:
+    permissions = profile.find(path=f'{permission_type}s[@{rule_type}="{rule_expression}"]')
+    if permissions is None:
+        permissions = etree.Element(permission_type + 's')
+        permissions.attrib[rule_type] = rule_expression
+        profile.append(permissions)
+    return permissions
+
+
+def add_permission(profile: etree._ElementTree, node: Node, permission_type: str, rule_type: str, rule_expression: str, expressions: Dict) -> None:
+    # get permission
+    permissions = get_permission(profile, permission_type, rule_type, rule_expression)
+
+    # add permission
+    for expression_name in expressions:
+        permission = etree.Element(permission_type)
+        if expression_name.startswith(node.name + '/'):
+            permission.text = '~' + expression_name[len(node.name):]
+        elif expression_name.startswith('/'):
+            permission.text = expression_name[len('/'):]
+        else:
+            permission.text = expression_name
+        permissions.append(permission)
+
+
 def convert_to_policy(policy_file_path: pathlib.Path, nodl_description: List[Node]) -> etree._ElementTree:
     policy = get_policy(policy_file_path)
 
     for node in nodl_description:
-        # profile: need to find node path somehow
+        # profile: need to find enclave path and node namespace somehow
+        profile = get_profile(policy, node.name)
 
-        # Below info can be extracted from `nodl.Node` class
-        # subscriptions
-        # publishers
-        # replies - server/action
-        # requests - server/action
-        # parameters
+        # parameters? Not specified in access control policy
+        subscribe_topics, publish_topics = _get_topics_by_role(node.topics)
+        reply_services, request_services = _get_services_by_role(node.services)
+        reply_actions, request_actions = _get_actions_by_role(node.actions)
 
-        # On this note, there is no mention of actions in
-        # `sros2/generate_policy.py` (https://github.com/ros2/sros2/blob/e5e90f05c3d800c51648e02d48d30ed4d6158820/sros2/sros2/verb/generate_policy.py#L124-L141)
-        # is this something to be looked at?
-        pass
+        if len(subscribe_topics) != 0:
+            add_permission(profile, node, 'topic', 'subscribe', 'ALLOW', subscribe_topics)
+        if len(publish_topics) != 0:
+            add_permission(profile, node, 'topic', 'publish', 'ALLOW', publish_topics)
+        if len(reply_services) != 0:
+            add_permission(profile, node, 'service', 'reply', 'ALLOW', reply_services)
+        if len(request_services) != 0:
+            add_permission(profile, node, 'service', 'request', 'ALLOW', request_services)
+        if len(reply_actions) != 0:
+            add_permission(profile, node, 'action', 'execute', 'ALLOW', reply_actions)
+        if len(request_actions) != 0:
+            add_permission(profile, node, 'action', 'call', 'ALLOW', request_actions)
 
     return policy
 
@@ -65,3 +125,36 @@ def convert_to_policy(policy_file_path: pathlib.Path, nodl_description: List[Nod
 def write_policy(policy_file_path: pathlib.Path, policy: etree._ElementTree) -> None:
     with open(policy_file_path, 'w') as stream:
         dump_policy(policy, stream)
+
+
+def _get_topics_by_role(topics: Optional[Dict]) -> Tuple[Dict, Dict]:
+    subscribe_topics = {}
+    publish_topics = {}
+    for _, topic in topics.items():
+        if PubSubRole(topic.role) is PubSubRole.SUBSCRIPTION:
+            subscribe_topics[topic.name] = topic
+        elif PubSubRole(topic.role) is PubSubRole.PUBLISHER:
+            publish_topics[topic.name] = topic
+        else:  # PubSubRole(topic.role) is PubSubRole.BOTH
+            subscribe_topics[topic.name] = topic
+            publish_topics[topic.name] = topic
+    return subscribe_topics, publish_topics
+
+
+def _get_services_by_role(services: Optional[Dict]) -> Tuple[Dict, Dict]:
+    reply_services = {}
+    request_services = {}
+    for _, service in services.items():
+        if ServerClientRole(service.role) is ServerClientRole.CLIENT:
+            request_services[service.name] = service
+        elif ServerClientRole(service.role) is ServerClientRole.SERVER:
+            reply_services[service.name] = service
+        else:  # ServerClientRole(service.role) is ServerClientRole.BOTH
+            request_services[service.name] = service
+            reply_services[service.name] = service
+
+    return reply_services, request_services
+
+
+def _get_actions_by_role(actions: Optional[Dict]) -> Tuple[Dict, Dict]:
+    return _get_services_by_role(actions)
